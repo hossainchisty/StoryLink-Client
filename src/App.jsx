5d696fed7d52e0c8b69fb874226f4ece8ec4497d
hossainchisty <hossain.chisty11@gmail.com>
2023-08-16 19:46:37 +0600

    feat: Linked post draft element

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./context/UserContext";
+import CreatePostPage from "./pages/CreatePostPage";
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
         } />
         <Route path={'/register'} element={
           <RegisterPage />
+        } />
+        <Route path={'/draft'} element={
+          <CreatePostPage/>
         } />
       </Route>
     </Routes>
