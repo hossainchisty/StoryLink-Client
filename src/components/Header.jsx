a9756af9dd9c6853c530063abc645a2c5864f942
hossainchisty <hossain.chisty11@gmail.com>
2023-08-18 02:50:44 +0600

    feat: Use usercontext for user info

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
-import {Link} from 'react-router-dom'
-import { useEffect, useContext} from 'react';
+import { Link } from 'react-router-dom'
+import { useEffect, useContext } from 'react';
 import { UserContext } from '../context/UserContext';
 
 
 export default function Header() {
-  const {setUserInfo, userInfo} = useContext(UserContext);
+  const { setUserInfo, userInfo } = useContext(UserContext);
   useEffect(() => {
     fetch('http://localhost:8000/api/v1/users/me', {
       credentials: 'include',
     }).then(response => {
-      response.json().then(userData =>{
+      response.json().then(userData => {
         setUserInfo(userData);
       });
     })
 
-  },);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   function logout() {
     fetch('http://localhost:8000/api/v1/users/logout', {
@@ -27,18 +28,18 @@ export default function Header() {
   const userName = userInfo?.full_name;
   return (
     <header>
-        <Link to="/" className="logo">
-          StoryLink
-        </Link>
-        
-        <Link to="/" className="feed-link">
-          My Feed
-        </Link>
-        
-        <Link to="/explore" className="feed-link">
-          Explore
-        </Link>
-        <nav>
+      <Link to="/" className="logo">
+        StoryLink
+      </Link>
+
+      <Link to="/" className="feed-link">
+        My Feed
+      </Link>
+
+      <Link to="/explore" className="feed-link">
+        Explore
+      </Link>
+      <nav>
         {userName && (
           <>
             <Link to="/draft" className='write-link'>Write</Link>
@@ -51,7 +52,7 @@ export default function Header() {
             <Link to="/register">Register</Link>
           </>
         )}
-        </nav>
-      </header>
+      </nav>
+    </header>
   )
 }
