c66925cab56e37d45339f25a8d57d1c9322cdc6b
hossainchisty <hossain.chisty11@gmail.com>
2023-08-18 02:52:02 +0600

    feat: Made dynamic content of post

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,12 +1,22 @@
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from 'react'
 import Post from '../components/Post'
 
+
 export default function IndexPage() {
+  const [posts,setPosts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/api/v1/posts/list').then(response => {
+      response.json().then(posts => {
+        setPosts(posts.data.posts);
+      });
+    });
+  }, []);
   return (
     <>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
+      {posts.length > 0 && posts.map(post => (
+        <Post {...post}/>
+      ))}
     </>
-  )
+  );
 }
