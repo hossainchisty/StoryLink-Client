66151601866bf22d289561a39197dc2571e73c28
hossainchisty <hossain.chisty11@gmail.com>
2023-08-18 12:03:27 +0600

    feat: Added new route for edit post

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./context/UserContext";
@@ -28,6 +29,10 @@ function App() {
         } />
         <Route path={'/post/:id'} element={
           <PostPage/>
+        } />
+        
+        <Route path={'/edit/:id'} element={
+          <EditPost/>
         } />
       </Route>
     </Routes>
