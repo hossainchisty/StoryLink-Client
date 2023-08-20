d4f0163f3668a1fd8e4b2394329532094fe24a4e
hossainchisty <hossain.chisty11@gmail.com>
2023-08-20 13:20:59 +0600

    perf: few improvements to enhance readability and maintainability

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,46 +1,59 @@
 import { useContext, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [redirect, setRedirect] = useState(false);
-  const {setUserInfo} = useContext(UserContext);
+  const { setUserInfo } = useContext(UserContext);
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const navigate = useNavigate(); // Get the navigate function
 
   async function login(event) {
     event.preventDefault();
     const requestBody = {
-      email: email,
-      password: password
-    }
-    const response = await fetch(`${apiBaseUrl}/users/login`, {
-      method: 'POST',
-      body: JSON.stringify(requestBody),
-      headers: { 'Content-Type': 'application/json' },
-      credentials: 'include',
-    });
-    if (response.status === 200) {
-      response.json().then(userInfo => {
-        setUserInfo(userInfo)
-        setRedirect(true);
-      })
-    } else {
-      alert('Wrong credentials')
-    }
-  }
+      email,
+      password,
+    };
 
-  if (redirect) {
-    return <Navigate to={'/'} />
+    try {
+      const response = await fetch(`${apiBaseUrl}/users/login`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+      });
+
+      if (response.status === 200) {
+        const userInfo = await response.json();
+        setUserInfo(userInfo);
+        navigate('/'); // Use navigate to redirect to the home page
+      } else {
+        alert('Wrong credentials');
+      }
+    } catch (error) {
+      console.error('Login error:', error);
+      alert('An error occurred while logging in');
+    }
   }
 
   return (
-    <form action="" onSubmit={login}>
+    <form onSubmit={login}>
       <h1>Login</h1>
-      <input type="email" placeholder="Enter your email address" value={email} onChange={event => setEmail(event.target.value)} />
-      <input type="password" placeholder="Enter your password" value={password} onChange={event => setPassword(event.target.value)} />
-      <button>Login</button>
+      <input
+        type="email"
+        placeholder="Enter your email address"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Enter your password"
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
+      <button type="submit">Login</button>
     </form>
-  )
+  );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
   const [fullName, setFullName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
 
+  const handleFullNameChange = event => setFullName(event.target.value);
+  const handleEmailChange = event => setEmail(event.target.value);
+  const handlePasswordChange = event => setPassword(event.target.value);
 
-  async function register(event) {
-    event.preventDefault();
-
+  async function sendRegistrationRequest() {
     const requestBody = {
       full_name: fullName,
       email: email,
@@ -23,22 +25,31 @@ export default function RegisterPage() {
       headers: { 'Content-Type': 'application/json' },
     });
 
+    return response;
+  }
+
+  async function register(event) {
+    event.preventDefault();
+
+    const response = await sendRegistrationRequest();
+
     if (response.status === 201) {
-      return <Navigate to={'/login'} />
-    } else if (response.status === 429) {
-      alert('Too Many Requests, please try again later');
-    }
-    else {
-      alert('Registration failed');
+      navigate('/login');
+    } else {
+      const alertMessage =
+        response.status === 429
+          ? 'Too Many Requests, please try again later'
+          : 'Registration failed';
+      alert(alertMessage);
     }
   }
 
   return (
     <form className="register" onSubmit={register}>
       <h1>Register</h1>
-      <input type="text" placeholder="Enter your name" value={fullName} onChange={event => setFullName(event.target.value)} />
-      <input type="email" placeholder="Enter your email address" value={email} onChange={event => setEmail(event.target.value)} />
-      <input type="password" placeholder="Enter your password" value={password} onChange={event => setPassword(event.target.value)} />
+      <input type="text" placeholder="Enter your name" value={fullName} onChange={handleFullNameChange} />
+      <input type="email" placeholder="Enter your email address" value={email} onChange={handleEmailChange} />
+      <input type="password" placeholder="Enter your password" value={password} onChange={handlePasswordChange} />
       <button>Register</button>
     </form>
   );
