c9349eed570f53d1c2694afac1da182302af6f03
hossainchisty <hossain.chisty11@gmail.com>
2023-08-26 21:44:08 +0600

    feat: Added ablity to search post by title

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,35 +1,50 @@
-import '../styles/Explore.css'; // Import your CSS file
-
-const dummyPosts = [
-  {
-    id: 1,
-    title: 'First Post',
-    content: 'This is the content of the first post.',
-    imageUrl: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1690175267986/da3e5ed2-6240-41b5-8a53-dcf1ac2bc541.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp', // Replace with your image URLs
-  },
-  {
-    id: 2,
-    title: 'Second Post',
-    content: 'This is the content of the second post.',
-    imageUrl: 'https://cdn.hashnode.com/res/hashnode/image/upload/v1690175267986/da3e5ed2-6240-41b5-8a53-dcf1ac2bc541.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp',
-  },
-  // Add more dummy posts as needed
-];
+import "../styles/Explore.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Explore() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const apiBaseDomain = import.meta.env.VITE_API_DOMAIN;
+
+  const fetchPosts = (searchTerm) => {
+    if (searchTerm.trim() === "") {
+      setSearchResult([]); // Clear search result if search term is empty
+      return;
+    }
+
+    fetch(`http://127.0.0.1:8000/api/v1/posts/search?title=${searchTerm}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setSearchResult(data.data.posts);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts(searchTerm);
+  }, [searchTerm]);
+
   return (
     <div className="explore-container">
       <div className="search-bar">
-        <input type="text" placeholder={'Start typing to search'} />
+        <input
+          type="text"
+          placeholder="Start typing to search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
       </div>
       <div className="post-list">
-        {dummyPosts.map(post => (
+        {searchResult.map((post) => (
           <div key={post.id} className="post-item">
             <div className="post-image">
-              <img src={post.imageUrl} alt="Post" />
+              <img src={`${apiBaseDomain}/${post.cover}`} alt={post.title} />
             </div>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
+            <h3>
+              <Link to={`/post/${post._id}`}>{post.title}</Link>
+            </h3>
           </div>
         ))}
       </div>
